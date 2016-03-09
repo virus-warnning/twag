c272c506e9526bb165e3bc3ee68af49a2b1f7a72
Raymond Wu <virus.warnning@gmail.com>
2016-03-09 18:41:13 +0800

    refs #2 更新凶宅資料
    * 凶宅 DB Schema
    * 凶宅空資料
    * 7200 以上資料完成

--- categories/unluckyhouse/syncid.py
+++ categories/unluckyhouse/syncid.py
@@ -1,18 +1,23 @@
 # -.- encoding: utf-8 -.-
-
-# http://unluckyhouse.com/external.php
-# rss > channel > item > link (t=...)
- 
 import httplib
 import re
+import sqlite3
 from xml.etree.ElementTree import ElementTree
 
+def dict_factory(cursor, row):  
+    d = {}  
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d 
+
+# 取得台灣凶宅網的最新討論串 id
+# http://unluckyhouse.com/external.php
+# rss > channel > item > link (t=...)
+latest_id = -1
 host = 'unluckyhouse.com'
 uri  = '/external.php'
 conn = httplib.HTTPConnection(host)
-latest_id = -1
 
-# 取得台灣凶宅網的最新討論串 id
 try:
 	conn.request('GET', uri)
 	resp = conn.getresponse()
@@ -21,12 +26,33 @@ try:
 		tree.parse(resp)
 		latest_url = tree.getroot().find('channel/item/link').text
 		m = re.match(r".+t=(\d+).+", latest_url)
-		latest_id = m.group(1)
+		latest_id = int(m.group(1))
 	conn.close()
 except:
 	pass
 
 # SQLite 同步台灣凶宅網的 id，資料採用預設值
+sync_id = 0
 if latest_id is not -1:
-	print latest_id
-	# TODO
+	con = sqlite3.connect('unluckyhouse.sqlite')
+	con.row_factory = dict_factory
+	cur = con.cursor()
+
+	sql = 'SELECT max(id) sync_id FROM unluckyhouse'
+	cur.execute(sql)
+	row = cur.fetchone()
+	row['sync_id']
+	if row['sync_id'] is not None:
+		sync_id = row['sync_id']
+
+	if sync_id<latest_id:
+		print('Add entries %d ~ %d' % (sync_id+1,latest_id))
+		diff = range(sync_id+1, latest_id+1)
+		for i in diff:
+			sql = 'INSERT INTO unluckyhouse(id) VALUES (?)'
+			con.execute(sql, (i, ))
+			con.commit()
+	else:
+		print('Already synchronized (%d)' % latest_id)
+
+	con.close()
--- categories/unluckyhouse/unluckyhouse.sql
+++ categories/unluckyhouse/unluckyhouse.sql
@@ -1,16 +1,18 @@
-CREATE TABLE unluckyhouse (
-	id INTEGER PRIMARY KEY,
-	age INTEGER,
-	age_unit CHAR(1) DEFAULT 'Y',
-	gender CHAR(1) DEFAULT 'F',
-	initative CHAR(1) DEFAULT 'S',
-	approach VARCHAR(20),
-	news TEXT,
-	area VARCHAR(10),
-	address VARCHAR(255),
-	datetime DATETIME,
-	state INTEGER DEFAULT 0,
-	lat DOUBLE DEFAULT 25.0,
-	lng DOUBLE DEFAULT 119.5,
-	mtime DATETIME DEFAULT (datetime('now', 'localtime'))
+CREATE TABLE `unluckyhouse` (
+	`id`	INTEGER,
+	`age`	INTEGER,
+	`age_unit`	CHAR(1) DEFAULT 'Y',
+	`gender`	CHAR(1) DEFAULT 'F',
+	`initative`	CHAR(1) DEFAULT 'S',
+	`approach`	VARCHAR(20),
+	`news`	TEXT,
+	`area`	VARCHAR(10),
+	`address`	VARCHAR(255),
+	`datetime`	DATETIME,
+	`state`	INTEGER DEFAULT 0,
+	`lat`	DOUBLE DEFAULT 25.0,
+	`lng`	DOUBLE DEFAULT 119.5,
+	`azimuth`	INTEGER DEFAULT 0,
+	`mtime`	DATETIME DEFAULT (datetime('now','localtime')),
+	PRIMARY KEY(id)
 );
