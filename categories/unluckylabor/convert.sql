76b50f384b3f04a21bc0ee9f28413e0e36cdbe6a
Raymond Wu <virus.warnning@gmail.com>
2016-05-12 16:57:20 +0800

    refs #5 日期格式最佳化

--- categories/unluckylabor/convert.sql
+++ categories/unluckylabor/convert.sql
@@ -1,4 +1,5 @@
 -- 匯入 CSV 為草稿
+.separator ','
 .import unluckylabor.csv draft
 
 -- 目標格式
@@ -18,21 +19,25 @@ CREATE TABLE IF NOT EXISTS unluckylabor (
 DELETE FROM unluckylabor;
 
 -- 對應到目標格式
+-- 其中時間格式轉換如下
+-- 1. 把 / 換成 -
+-- 2. 年的前面補零，SQLite 只支援 YYYY-MM-DD 格式
+-- 3. 年 +1911
 INSERT INTO unluckylabor (
-	exe_id,
-	corperation,
-	detail,
-	ref_law,
-	boss,
-	exe_date
+    exe_id,
+    corperation,
+    detail,
+    ref_law,
+    boss,
+    exe_date
 ) SELECT
-    "處分字號",
-    "事業單位名稱/自然人姓名",
-    "違反法規內容",
-    "違反勞動基準法條款",
-    "事業單位代表人",
-    "處分日期"
-FROM draft ORDER BY "處分字號";
+    `處分字號`,
+    `事業單位名稱/自然人姓名`,
+    `違反法規內容`,
+    `違反勞動基準法條款`,
+    `事業單位代表人`,
+    date('0' || REPLACE(`處分日期`, '/', '-'), '+1911 years')
+FROM draft ORDER BY `處分字號`;
 
 -- 刪除草稿
 DROP TABLE draft;
