fca0993f7ccb5436cbb288e46cc80f23b9ae4c6e
Raymond Wu <virus.warnning@gmail.com>
2016-05-24 10:53:51 +0800

    refs #5 改良定位程式
    * 使用 geocoder 的 TGOS 功能
    * 完成 1~500 定位

--- categories/unluckylabor/geocoding.py
+++ categories/unluckylabor/geocoding.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import json
 
 commons_path = os.path.realpath('../../commons')
 sys.path.insert(1, commons_path)
@@ -11,23 +12,32 @@ import corp_utils
 import smart_dbapi
 from print_progress import print_progress
 
-LIMIT = 10
+BEGIN = 401
+LIMIT = 100
 
 rank = 0
-sql  = 'SELECT id,corperation FROM unluckylabor WHERE lat=0.0 AND lng=0.0 LIMIT ?'
+sql  = 'SELECT id,corperation FROM unluckylabor WHERE lat=0 AND lng=0 AND id>=? LIMIT ?'
 conn = smart_dbapi.connect('unluckylabor.sqlite')
-curr = conn.execute(sql, (LIMIT,))
+curr = conn.execute(sql, (BEGIN, LIMIT))
+
+changes = {}
 for row in curr:
 	rank = rank + 1
-	msg = u'處理中 %s (%d/%d) ...' % (row['corperation'], rank, LIMIT)
+	msg = u'定位中 %s (%d/%d) ...' % (row['corperation'], rank, LIMIT)
 	print_progress(msg)
 	sys.stdout.flush()
 
 	point = corp_utils.get_corp_location(row['corperation'])
 	if point != False:
-		conn.execute('UPDATE unluckylabor SET lat=?, lng=? WHERE id=?', (point[0], point[1], row['id']))
-		conn.commit()
-conn.close()
+		changes[row['id']] = point
 
 print('')
+print('更新資料庫')
+for i in changes:
+	(lat, lng) = changes[i]
+	conn.execute('UPDATE unluckylabor SET lat=?, lng=? WHERE id=?', (lat, lng, i))
+conn.commit()
+
+conn.close()
+
 print(u'完成')
--- commons/smart_geo.py
+++ commons/smart_geo.py
@@ -1,80 +1,21 @@
 #!../../bin/python
 # coding: utf-8
 #
-# 相依套件:
-# *       requests: 進行有狀態 HTTP 連線
-# *         pyproj: EPSG:3826 轉 EPSG:4326
-# * beautifulsoup4: 分解 HTML 原始碼內含的 pagekey
-#
 # @author 小璋丸 <virus.warnning@gmail.com>
 #
 
-import urllib
-import requests
-import pyproj
-import re
-import json
-from bs4 import BeautifulSoup
+import geocoder
 
-# TGOS 取得門牌座標
+# 使用 TGOS 定位
 def geocode(address):
-	pagekey = False
-	headers = {
-		'Origin': 'http://map.tgos.nat.gov.tw',
-		'Referer': 'http://map.tgos.nat.gov.tw/TGOSCLOUD/Web/Map/TGOSViewer_Map.aspx',
-		'X-Requested-With': 'XMLHttpRequest'
-	}
-
-	# 弄到 pagekey, cookies 產生合理狀態
-	# *   range: <script id='sircMessage1'>...</script>
-	# * pattern: window.sircMessage.sircPAGEKEY = '...';
-	url = 'http://map.tgos.nat.gov.tw/TGOSCLOUD/Web/Map/TGOSViewer_Map.aspx'
-	r = requests.post(url)
-	if r.status_code == 200:
-		soup = BeautifulSoup(r.text, 'html.parser')
-		node = soup.find('script', {'id': 'sircMessage1'})
-		script = node.get_text().strip()
-		m = re.search('window\.sircMessage\.sircPAGEKEY\s?=\s?\'([\w\+%]+)\';', script)
-		if m != None:
-			pagekey = urllib.unquote(m.group(1))
-			cookies = {}
-			for c in r.cookies:
-				cookies[c.name] = c.value
-
-	if pagekey == False: return False
-
-	# 查詢前的 Request (TGOS 介面上有，而實際上是多餘的動作)
-	'''
-	url = 'http://map.tgos.nat.gov.tw/TGOSCLOUD/Generic/Utility/UG_Handler.ashx?method=GetSessionID&pagekey=' + pagekey
-	r = requests.post(url, headers=headers, cookies=cookies)
-	if r.status_code != 200 or r.json()['success'] != 'true':
-		return False
-	'''
-
-	# 查詢精確位置 (TWD97)
-	loc3826 = False
-	url = 'http://map.tgos.nat.gov.tw/TGOSCloud/Generic/Project/GHTGOSViewer_Map.ashx?pagekey=' + pagekey
-	params = {
-		'method': 'queryaddr',
-		'useoddeven': 'false',
-		'address': address,
-		'sid': cookies['ASP.NET_SessionId']
-	}
-	r = requests.post(url, data=params, cookies=cookies, headers=headers)
-	if r.status_code == 200 and 'AddressList' in r.json():
-		result  = r.json()['AddressList'][0]
-		loc3826 = (result['X'], result['Y'])
-
-	if loc3826 == False: return False
-
-	# TWD97 轉 WGS84, 回傳 (緯度, 經度)
-	twd97   = pyproj.Proj(init='EPSG:3826')
-	loc4326 = twd97(loc3826[0], loc3826[1], inverse=True)
-	return (loc4326[1], loc4326[0])
+	g = geocoder.tgos(address)
+	if g.parse.get('featureCount') > 0:
+		return (g.lat, g.lng)
+	return False
 
 # 簡易測試
 def main():
-	loc = geocode('台北市內湖區內湖路一段591號')
+	loc = geocode('台北市內湖區內湖路一段735號')
 	if loc != False:
 		print('(%f, %f)' % loc)
 
