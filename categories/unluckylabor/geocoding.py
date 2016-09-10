0c530dfb73b129afd0faddb3236ce1ad08464451
Raymond Wu <virus.warnning@gmail.com>
2016-09-11 00:58:19 +0800

    refs #8 公司資訊 API 重構

--- categories/unluckylabor/geocoding.py
+++ categories/unluckylabor/geocoding.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import json
+import time
 
 commons_path = os.path.realpath('../../commons')
 sys.path.insert(1, commons_path)
@@ -12,33 +13,35 @@ import corp_utils
 import smart_dbapi
 from print_progress import print_progress
 
-BEGIN = 0
-LIMIT = 5000
+BEGIN = 891
+LIMIT = 10
 
 rank = 0
 sql  = 'SELECT id,corp,boss FROM unluckylabor WHERE lat=0 AND lng=0 AND id>=? LIMIT ?'
 conn = smart_dbapi.connect('unluckylabor.sqlite')
 curr = conn.execute(sql, (BEGIN, LIMIT))
 
+# 蒐集要修改項目
+cnt = 0
 changes = {}
 for row in curr:
 	rank = rank + 1
-	msg = u'定位中 %s (%d/%d) ...' % (row['corp'], rank, LIMIT)
+	info = corp_utils.get_corp_info(row['corp'])
+	if info != False and info['lat'] != 0:
+		changes[row['id']] = info
+		cnt = cnt + 1
+
+	msg = u'處理中 #%d %s (%d/%d) 需要更新 %d 項 ...\n' % (row['id'], row['corp'], rank, LIMIT, cnt)
 	print_progress(msg)
 	sys.stdout.flush()
+	time.sleep(0.5)
 
-	info = corp_utils.get_corp_info(row['corp'])
-	if info != False:
-		changes[row['id']] = info
-
-print('')
-print(len(changes))
-print('更新資料庫')
+# 更新
+print('\n更新資料庫')
 for i in changes:
 	info = changes[i]
 	conn.execute('UPDATE unluckylabor SET lat=?, lng=? WHERE id=?', (info['lat'], info['lng'], i))
 
-print(info)
 conn.commit()
 conn.close()
 
--- commons/corp_utils.py
+++ commons/corp_utils.py
@@ -2,9 +2,8 @@
 # coding: utf-8
 
 import os
-import re
-import httplib
-import urllib
+import json
+import sys
 import smart_http
 import smart_geo
 import smart_dbapi
@@ -12,53 +11,100 @@ import smart_dbapi
 DEBUG = True
 CODEPATH = os.path.dirname(os.path.abspath(__file__))
 
-## 查詢公司統編
-#  - 利用 RonnyWang 的公司名稱查詢
-#  - 查詢公司名稱
-#  - 取重新導向的 header location
-#  - 從 location 取出 id
-def get_corp_id(name):
-	corp_id = False
+## 查詢公司資訊
+# 取統編、名稱、負責人、地址、登記機關五個欄位
+def get_corp_info(name, boss, gov):
+	corp_info = False
 
 	try:
 		if type(name) is unicode:
 			name = name.encode('utf-8')
 
-		conn  = httplib.HTTPConnection('company.g0v.ronny.tw')
-		query = urllib.urlencode({'q': name})
-		uri   = '/index/search?%s' % query
-		
-		conn.request('GET', uri)
-		resp  = conn.getresponse()
+		# http://company.g0v.ronny.tw/api/search?q=
+		resp = smart_http.request('company.g0v.ronny.tw', '/api/search', {'q': name})
+		if resp != False and resp['found'] > 0:
+			# 計算每筆資料的相似度績分
+			# 名稱 5
+			# 負責人 2
+			# 註冊單位 1
+			# 核准設立 1
+			score_list = []
+			for e in resp['data']:
+				score = 0
 
-		if resp.status == 302:
-			moveto  = resp.getheader('Location', '')
-			match   = re.match('/id/(\d+)', moveto)
-			if match != None:
-				corp_id = match.group(1)
+				# 名稱
+				if u'商業名稱' in e and e[u'商業名稱'] == name:
+					score = score + 5
+
+				# 負責人
+				#if u'負責人姓名' in e and e[u'負責人姓名'] == boss:
+				#	score = score + 2
+
+				# 註冊單位
+				#if u'登記機關' in e and e[u'登記機關'] == gov:
+				#	score = score + 1
+
+				# 核准設立
+				if u'現況' in e and e[u'現況'] == u'核准設立':
+					score = score + 1
+
+				uid = e[u'統一編號']
+				print(u'%s %s: 積分 %d' % (uid, name, score))
+				score_list.append(score)
+			'''
+			if resp['found'] == 1:
+				corp_info = resp['data'][0] # [u'統一編號']
 			else:
-				if DEBUG: print('invalid request')
-		else:
-			if DEBUG: print('Response Code: %d' % resp.status)
+				# TODO: 出現同名公司，依下列順序選取一項
+				# 1. 負責人姓名吻合
+				# 2. 註冊單位吻合
+				# 3. 核准設立
+				for e in resp['data']:
+					if u'公司狀況' in e and e[u'公司狀況'] == u'核准設立':
+						corp_info = e # [u'統一編號']
+					if u'現況' in e and e[u'現況'] == u'核准設立':
+						corp_info = e # [u'統一編號']
+			'''
 
-		conn.close()
-	except Exception as e:
-		if DEBUG: print(e)
+	except Exception:
+		if DEBUG:
+			errln = sys.exc_info()[-1].tb_lineno
+			print('第 %d 行發生錯誤' % errln)
 
-	return corp_id
+	return corp_info
 
-# 取得公司資訊
-def get_corp_info(name):
+## 取得公司資訊
+#
+# @param name 事業名稱
+# @param boss 負責人姓名
+# @param gov  登記機關
+#
+'''
+def get_corp_info(name, boss='', gov=''):
 	dbfile = '%s/corp_cache.sqlite' % CODEPATH
 	conn = smart_dbapi.connect(dbfile)
 
-	sql = 'SELECT name,uid,boss,addr,lat,lng FROM corp_cache WHERE name=?'
+	sql = 'SELECT name,uid,boss,addr,lat,lng,mtime FROM corp_cache WHERE name=?'
 	cur = conn.execute(sql, (name,))
+	# TODO: 如果快取出現多筆，依下列順序選取一項
+	# 1. 負責人姓名吻合
+	# 2. 註冊單位吻合
+	# 3. 其他
 	info = cur.fetchone()
 	cur.close()
 
+	# TODO: 如果 lat = 0.0 七天後再試一次
+	#       如果 lat > 0.0 一年後再試一次
+
+	# 先用暴力法刪除有公司無地址項目，強迫重新查一次
+	if info is not None and (info['addr'] == None or info['addr'] == ''):
+		conn.execute('DELETE FROM corp_cache WHERE name=?', [name])
+		conn.commit()
+		info = None
+
+	# 沒有快取資料可以用，用 API 查一下
 	if info is None:
-		uid = get_corp_id(name)
+		uid = get_corp_id(name, boss, gov)
 		if uid == False:
 			sql = 'INSERT INTO corp_cache(name,mtime) VALUES(?,DATETIME())'
 			conn.execute(sql, (name,))
@@ -89,6 +135,15 @@ def get_corp_info(name):
 				if u'代表人姓名' in resp['data']:
 					boss = resp['data'][u'代表人姓名']
 
+				print(addr)
+
+				if addr != '' and loc == False:
+					print('有地址卻定位失敗，可能是 TGOS 禁止存取: %s' % addr)
+					exit(0)
+			else:
+				print('Ronny API return False')
+
+			# 紀錄到快取
 			sql = 'INSERT INTO corp_cache(name,uid,boss,addr,lat,lng,mtime) VALUES(?,?,?,?,?,?,DATETIME())'
 			conn.execute(sql, (name, uid, boss, addr, lat, lng))
 			conn.commit()
@@ -105,10 +160,16 @@ def get_corp_info(name):
 	conn.close()
 
 	return info
+'''
 
 ## 簡易測試
 def main():
-	print(get_corp_info(u'廣全科技股份有限公司'))
+	#print(get_corp_id(u'興富發建設股份有限公司'))
+	print(json.dumps(get_corp_info(u'萬相企業社', '', ''),ensure_ascii=False,indent=2))
+	print(json.dumps(get_corp_info(u'萬相企業社', '郭同會', '桃園縣政府'),ensure_ascii=False,indent=2))
+	print(json.dumps(get_corp_info(u'萬相企業社', '洪美英', '桃園縣政府'),ensure_ascii=False,indent=2))
+	#print(get_corp_id(u'去你爸股份有限公司'))
+	#print(get_corp_info(u'廣全科技股份有限公司'))
 
 if __name__ == "__main__":
 	main()
